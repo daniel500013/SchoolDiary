8b77d9728e8d1a10d6ed541ecc0f49651e7d279e
daniel500013 <bakucraft229@gmail.com>
2022-07-12 00:47:06 +0200

    initialize

---
+++ SchoolDiary.api/SchoolDiary.api/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolDiary.api.Model
+{
+    public class Person
+    {
+        [Key]
+        public Guid UserUUID { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordHashed { get; set; }
+    }
+}
