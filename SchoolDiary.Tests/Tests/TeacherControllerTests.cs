39b6d2493b00c0b21c973ccb5aac70393b3d2917
daniel500013 <bakucraft229@gmail.com>
2022-07-15 05:30:49 +0200

    TeacherController Tests

---
+++ SchoolDiary.Tests/Tests/TeacherControllerTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.Tests.Tests
+{
+    public class TeacherControllerTests
+    {
+        private HttpClient Client { get; set; }
+
+        public TeacherControllerTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            Client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContext = services
+                            .SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<DiaryDbContext>));
+                        services.Remove(dbContext);
+
+                        services.AddSingleton<IPolicyEvaluator, FakePolicy>();
+
+                        services.AddDbContext<DiaryDbContext>(options => options.UseInMemoryDatabase("DiaryDb"));
+                    });
+                })
+                .CreateClient();
+        }
+
+        [Fact]
+        public async Task GetTeachers_WithoutParams_ReturnOkRequest()
+        {
+            var response = await Client.GetAsync("/api/Teacher");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CreateTeacher_WithoutParams_ReturnOkRequest()
+        {
+            var model = new TeacherViewModel()
+            {
+                FirstName = "Jon",
+                LastName = "Testman",
+                Gender = true,
+                Email = "jon.testman@gmail.com",
+                Phone = "123456789"
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync("/api/Teacher", httpContext);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task ChangeTeacher_WithoutParams_ReturnOkRequest()
+        {
+            var ModelToAdd = new TeacherViewModel()
+            {
+                FirstName = "Jon",
+                LastName = "Testman",
+                Gender = true,
+                Email = "jon.testman@gmail.com",
+                Phone = "123456789"
+            };
+
+            var jsonToAdd = JsonConvert.SerializeObject(ModelToAdd);
+
+            var httpContextToAdd = new StringContent(jsonToAdd, Encoding.UTF8, "application/json");
+
+            await Client.PostAsync("/api/Teacher", httpContextToAdd);
+
+            var model = new TeacherViewModel()
+            {
+                FirstName = "Henry",
+                LastName = "NoTestman",
+                Gender = false,
+                Email = "jon.notestman@gmail.com",
+                Phone = "987654321"
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await Client.PutAsync("/api/Teacher/1", httpContext);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task DeleteTeacher_WithoutParams_ReturnOkRequest()
+        {
+            var model = new TeacherViewModel()
+            {
+                FirstName = "Jon",
+                LastName = "Testman",
+                Gender = true,
+                Email = "jon.testman@gmail.com",
+                Phone = "123456789"
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+            await Client.PostAsync("/api/Teacher", httpContext);
+            await Client.PostAsync("/api/Teacher", httpContext);
+
+            var response = await Client.DeleteAsync("/api/Teacher/2");
+
+            await response.Content.ReadAsStringAsync();
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
