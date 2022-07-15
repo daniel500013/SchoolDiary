5d55cf87f1b5e92a4b3f4edde590a4c81b169bbe
daniel500013 <bakucraft229@gmail.com>
2022-07-15 21:03:11 +0200

    Mark Tests

---
+++ SchoolDiary.Tests/Tests/MarkControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.Tests.Tests
+{
+    public class MarkControllerTests
+    {
+        private HttpClient Client { get; set; }
+
+        public MarkControllerTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            Client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContext = services
+                            .SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<DiaryDbContext>));
+                        services.Remove(dbContext);
+
+                        services.AddSingleton<IPolicyEvaluator, FakePolicy>();
+
+                        services.AddDbContext<DiaryDbContext>(options => options.UseInMemoryDatabase("DiaryDb"));
+                    });
+                })
+                .CreateClient();
+        }
+
+        [Fact]
+        public async Task GetMark_WithoutParams_ReturnOkRequest()
+        {
+            var response = await Client.GetAsync("/api/Mark");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CreateMark_WithoutParams_ReturnOkRequest()
+        {
+            var model = new MarkViewModel()
+            {
+                Present = false
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync("/api/Mark", httpContext);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task ChangeMark_WithoutParams_ReturnOkRequest()
+        {
+            var model = new MarkViewModel()
+            {
+                Present = false
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+            await Client.PostAsync("/api/Mark", httpContext);
+
+            var ChangeModel = new MarkViewModel()
+            {
+                Present = true
+            };
+
+            var JsonToChange = JsonConvert.SerializeObject(model);
+            var HttpContextToChange = new StringContent(JsonToChange, Encoding.UTF8, "application/json");
+            var response = await Client.PutAsync("/api/Mark/1", HttpContextToChange);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task DeleteMark_WithoutParams_ReturnOkRequest()
+        {
+            var model = new MarkViewModel()
+            {
+                Present = false
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+            await Client.PostAsync("/api/Mark", httpContext);
+
+            var ChangeModel = new MarkViewModel()
+            {
+                Present = true
+            };
+
+            var response = await Client.DeleteAsync("/api/Mark/1");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
---
+++ SchoolDiary.api/Controllers/GradeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolDiary.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GradeController : ControllerBase
+    {
+        public GradeController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllGrades()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGrade()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ChangeGrade()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteGrade()
+        {
+            return Ok();
+        }
+    }
+}
