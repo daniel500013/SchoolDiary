f6aa29429e5dadbb0118a67f774bf01a327c39e0
daniel500013 <bakucraft229@gmail.com>
2022-07-16 01:33:03 +0200

    Approve tests

---
+++ SchoolDiary.Tests/Tests/ApproveControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.Tests.Tests
+{
+    public class ApproveControllerTests
+    {
+        private HttpClient Client { get; set; }
+
+        public ApproveControllerTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            Client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContext = services
+                            .SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<DiaryDbContext>));
+                        services.Remove(dbContext);
+
+                        services.AddSingleton<IPolicyEvaluator, FakePolicy>();
+
+                        services.AddDbContext<DiaryDbContext>(options => options.UseInMemoryDatabase("DiaryDb"));
+                    });
+                })
+                .CreateClient();
+        }
+
+        [Fact]
+        public async Task GetApproves_WithoutParams_ReturnOkRequest()
+        {
+            var response = await Client.GetAsync("/api/Approve");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CreateApprove_WithoutParams_ReturnOkRequest()
+        {
+            var model = new ApproveViewModel()
+            {
+                Positive = true,
+                Description = "desc",
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await Client.PostAsync("/api/Approve", httpContext);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task ChangeApprove_WithoutParams_ReturnOkRequest()
+        {
+            var model = new ApproveViewModel()
+            {
+                Positive = true,
+                Description = "desc",
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+            await Client.PostAsync("/api/Approve", httpContext);
+            await Client.PostAsync("/api/Approve", httpContext);
+
+            var ModelToChange = new ApproveViewModel()
+            {
+                Positive = false,
+                Description = "desc",
+            };
+
+            var JsonToChange = JsonConvert.SerializeObject(ModelToChange);
+            var HttpContextToChange = new StringContent(JsonToChange, Encoding.UTF8, "application/json");
+            var response = await Client.PutAsync("/api/Approve/1", HttpContextToChange);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task RemoveApprove_WithoutParams_ReturnOkRequest()
+        {
+            var model = new ApproveViewModel()
+            {
+                Positive = true,
+                Description = "desc",
+            };
+
+            var json = JsonConvert.SerializeObject(model);
+            var httpContext = new StringContent(json, Encoding.UTF8, "application/json");
+            await Client.PostAsync("/api/Approve", httpContext);
+
+            var response = await Client.DeleteAsync("/api/Approve/1");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
--- SchoolDiary.api/Controllers/ApproveController.cs
+++ SchoolDiary.api/Controllers/ApproveController.cs
@@ -5,6 +5,7 @@ namespace SchoolDiary.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ApproveController : ControllerBase
     {
         private readonly ApproveService ApproveService;
@@ -14,6 +15,10 @@ namespace SchoolDiary.api.Controllers
             ApproveService = approveService;
         }
 
+        /// <summary>
+        /// Return list of approves
+        /// </summary>
+        [Authorize(Roles = "Tutor,LocalAdmin,Admin")]
         [HttpGet]
         public async Task<IActionResult> GetAllApproves()
         {
@@ -22,6 +27,20 @@ namespace SchoolDiary.api.Controllers
             return Ok(approves);
         }
 
+        /// <summary>
+        /// Create Approve in diary
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Approve
+        ///     {
+        ///        "positive": true, - Positivity of approve
+        ///        "description": "Approve desc" - Approve description
+        ///     }
+        ///
+        /// </remarks>
+        [Authorize(Roles = "Teacher,Tutor,LocalAdmin,Admin")]
         [HttpPost]
         public async Task<IActionResult> CreateApprove(ApproveViewModel approve)
         {
@@ -30,6 +49,20 @@ namespace SchoolDiary.api.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Change Approve in diary by specify id
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /Approve/{id}
+        ///     {
+        ///        "positive": true, - Positivity of approve
+        ///        "description": "Approve desc" - Approve description
+        ///     }
+        ///
+        /// </remarks>
+        [Authorize(Roles = "Teacher,Tutor,LocalAdmin,Admin")]
         [HttpPut]
         [Route("{id:int}")]
         public async Task<IActionResult> ChangeApprove(int id, ApproveViewModel approve)
@@ -39,6 +72,10 @@ namespace SchoolDiary.api.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Remove Approve from diary by specify id
+        /// </summary>
+        [Authorize(Roles = "Teacher,Tutor,LocalAdmin,Admin")]
         [HttpDelete]
         [Route("{id:int}")]
         public async Task<IActionResult> DeleteApprove(int id)
