0836e4fcc219c71a5d40c127e99511877c1c05f1
daniel500013 <bakucraft229@gmail.com>
2022-07-16 03:53:45 +0200

    GradeManager tests

---
+++ SchoolDiary.Tests/Tests/GradeManagerControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.Tests.Tests
+{
+    public class GradeManagerControllerTests
+    {
+        private HttpClient Client { get; set; }
+
+        public GradeManagerControllerTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            Client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContext = services
+                            .SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<DiaryDbContext>));
+                        services.Remove(dbContext);
+
+                        services.AddSingleton<IPolicyEvaluator, FakePolicy>();
+
+                        services.AddDbContext<DiaryDbContext>(options => options.UseInMemoryDatabase("DiaryDb"));
+                    });
+                })
+                .CreateClient();
+        }
+
+        [Fact]
+        public async Task GetLessonGrades_WithoutParams_ReturnOkRequest()
+        {
+            var response = await Client.GetAsync("/api/GradeManager");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
